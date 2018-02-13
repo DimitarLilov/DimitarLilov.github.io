b77935afc466dc4670056bbde5b9c5c4e8902ab8
Dimitar Lilov <dimitar.lilov.mici@gmail.com>
2018-02-13 22:11:12 +0200

    Added projects folder

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,20 @@
+const handlers = {};
+
 $(() => {
     const app = Sammy('#content', function () {
         this.use("Handlebars", 'hbs');
 
         this.get('index.html', displayHome);
         this.get('/', displayHome);
-        this.get('/aboutme', displayAboutMe)
+        this.get('/aboutme', handlers.displayAboutMe);
+        this.get('/aboutme/resume', handlers.displayAboutMeFile);
+        this.get('/projects', handlers.displayProjectsFolder);
 
         function displayHome(ctx) {
+            ctx.aboutMeFolder = "fa fa-folder fa-5x";
+            ctx.projectsFolder = "fa fa-folder fa-5x";
+            ctx.educationFolder = "fa fa-folder fa-5x";
+            ctx.certificatesFolder = "fa fa-folder fa-5x";
             ctx.loadPartials({
                 header: "./templates/common/header.hbs",
                 footer: "./templates/common/footer.hbs",
@@ -15,17 +23,6 @@ $(() => {
                 this.partial('./templates/home/index.hbs');
             });
         }
-
-        function displayAboutMe(ctx){
-            ctx.loadPartials({
-                header: "./templates/common/header.hbs",
-                footer: "./templates/common/footer.hbs",
-
-            }).then(function () {
-                this.partial('./templates/home/aboutme.hbs');
-            });
-        }
-
     });
 
     app.run();
---
+++ scripts/handlers/aboutMeManager.js
@@ -0,0 +1,27 @@
+handlers.displayAboutMeFile = function (ctx) {
+    ctx.aboutMeFolder = "fa fa-folder-open fa-5x";
+            ctx.projectsFolder = "fa fa-folder fa-5x";
+            ctx.educationFolder = "fa fa-folder fa-5x";
+            ctx.certificatesFolder = "fa fa-folder fa-5x";
+            ctx.loadPartials({
+                header: "./templates/common/header.hbs",
+                footer: "./templates/common/footer.hbs",
+
+            }).then(function () {
+                this.partial('./templates/home/aboutMeFile.hbs');
+            });
+};
+
+handlers.displayAboutMe = function (ctx){
+    ctx.aboutMeFolder = "fa fa-folder-open fa-5x";
+    ctx.projectsFolder = "fa fa-folder fa-5x";
+    ctx.educationFolder = "fa fa-folder fa-5x";
+    ctx.certificatesFolder = "fa fa-folder fa-5x";
+    ctx.loadPartials({
+        header: "./templates/common/header.hbs",
+        footer: "./templates/common/footer.hbs",
+
+    }).then(function () {
+        this.partial('./templates/home/aboutme.hbs');
+    });
+};
