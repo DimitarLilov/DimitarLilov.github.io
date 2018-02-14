79b4e1dcf955054d9d2b29e363b9445cbdc0a5d9
Dimitar Lilov <dimitar.lilov.mici@gmail.com>
2018-02-14 14:52:09 +0200

    Added Contacts

--- scripts/handlers/aboutMeManager.js
+++ scripts/handlers/aboutMeManager.js
@@ -17,9 +17,17 @@ handlers.displayAboutMe = function (ctx) {
     ctx.projectsFolder = "fa fa-folder fa-5x";
     ctx.educationFolder = "fa fa-folder fa-5x";
     ctx.certificatesFolder = "fa fa-folder fa-5x";
+
+
+    ctx.id = "Photo";
+    ctx.name = "Photo.jpg";
+    ctx.url = "https://raw.githubusercontent.com/DimitarLilov/DimitarLilov.github.io/master/img/I.jpg";
+
+
     ctx.loadPartials({
         header: "./templates/common/header.hbs",
         footer: "./templates/common/footer.hbs",
+        photo: "./templates/projects/photo.hbs"
 
     }).then(function () {
         this.partial('./templates/home/aboutme.hbs');
